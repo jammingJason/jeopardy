9084785cb8f2b6f584a08b3c9def2647f0402204
Van Jason Crews <vanjasoncrews@gmail.com>
2022-08-23 08:41:51 -0400

    good working example

--- jeopardy.js
+++ jeopardy.js
@@ -21,14 +21,17 @@
 const h1 = document.createElement('h1');
 const div = document.createElement('div');
 const btn = document.createElement('button');
+const p = document.createElement('p');
 btn.className = 'btn btn-success btn-lg text-center';
 btn.id = 'btnStart';
 btn.innerText = 'Start/Reset Game';
 const body = document.querySelector('body');
-h1.innerText = 'Jeopardy';
+p.innerText = 'Jeopardy';
 h1.className = 'display-2 p-2 text-center';
-h1.id = 'header';
+p.id = 'header';
+p.className = 'lead text-lg';
 body.append(h1);
+h1.append(p);
 body.append(div);
 div.className = 'container-fluid text-center';
 div.append(btn);
@@ -46,7 +49,7 @@ let categories = [];
  */
 
 async function getCategoryIds() {
-  const rndNum = Math.floor(Math.random() * 1000);
+  const rndNum = Math.floor(Math.random() * 1456);
   const getIDs = await axios.get(
     'http://jservice.io/api/categories?count=6&offset=' + rndNum
   );
@@ -57,35 +60,24 @@ async function getCategoryIds() {
   getCategory(categories);
 }
 
-/** Return object with data about a category:
- *
- *  Returns { title: "Math", clues: clue-array }
- *
- * Where clue-array is:
- *   [
- *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
- *      {question: "Bell Jar Author", answer: "Plath", showing: null},
- *      ...
- *   ]
- */
-
 function getCategory(catId) {
-  const newTable = document.createElement('table');
   const tableDiv = document.createElement('div');
+  const rowDiv = document.createElement('div');
   body.append(tableDiv);
-  tableDiv.append(newTable);
-  tableDiv.className = 'container-fluid p-3';
-  newTable.className = 'table shadow text-center';
+  tableDiv.append(rowDiv);
+  tableDiv.className = 'container-fluid p-3 ';
+  tableDiv.id = 'tableDiv';
+  rowDiv.className = 'row text-center';
   // newTable.border = '1';
-  newTable.innerHTML =
-    '<thead id=tableHeader><tr id=headerTR class=tr></tr></thead>';
-  const getTR = document.querySelector('#headerTR');
 
+  showLoadingView();
   //   console.log(catId);
   for (let i = 0; i < catId.length; i++) {
-    const newTD = document.createElement('td');
-    getTR.append(newTD);
-    newTD.innerText = catId[i].title.toUpperCase();
+    const colDiv = document.createElement('div');
+    colDiv.className =
+      'col divHeaders d-flex align-items-center justify-content-center';
+    rowDiv.append(colDiv);
+    colDiv.innerText = catId[i].title.toUpperCase();
     // newTD.style.width = '17.5%';
   }
   fillTable(catId);
@@ -100,19 +92,72 @@ function getCategory(catId) {
  */
 
 async function fillTable(questions) {
-  //   console.log(questions[0].id);
-  const getQuestion = await axios.get(
-    'http://jservice.io/api/clues?value=5&category=271'
-  );
-  console.log(getQuestion);
-  const getTR = document.querySelector('#tableHeader');
-  const newTBody = document.createElement('tbody');
-  const newTR = document.createElement('tr');
-  getTR.append(newTBody);
-  newTBody.append(newTR);
+  const getTableDiv = document.querySelector('#tableDiv');
+  for (let i = 0; i < 5; i++) {
+    const newDiv = document.createElement('div');
+    newDiv.className = 'row ';
+    newDiv.id = i;
+    newDiv.style.backgroundColor = 'blue';
+    getTableDiv.append(newDiv);
+  }
+  let getQuestion = [];
+  for (let y = 0; y < questions.length; y++) {
+    const oneQuestion = await axios.get(
+      'http://jservice.io/api/clues?&category=' + questions[y].id
+    );
+    getQuestion.push(oneQuestion.data);
+  }
+  hideLoadingView();
+  // fillQuestions();
+  for (let j = 0; j < 5; j++) {
+    const getNewDiv = document.getElementById(j);
+    for (let v = 0; v < 6; v++) {
+      const newDiv = document.createElement('div');
+      newDiv.className =
+        'col divQuestions  d-flex align-items-center justify-content-center';
+      newDiv.id = v.toString() + j.toString();
+      newDiv.innerText = '$' + (j + 1) + '00';
+      getNewDiv.append(newDiv);
+      newDiv.addEventListener('click', function () {
+        // // alert(newDiv.id);
+        // const getDivID = document.getElementById(newDiv.id);
+        // getDivID.style.backgroundColor = 'white';
 
-  const newTD = document.createElement('td');
-  newTR.append(newTD);
+        fillQuestions(j, v);
+      });
+    }
+  }
+  // for (let j = 0; j < 5; j++) {
+  //   const getNewTR = document.getElementById(j);
+  //   for (let v = 0; v < 6; v++) {
+  //     const makeNewTD = document.createElement('td');
+  //     makeNewTD.innerText = '?';
+  //     getNewTR.append(makeNewTD);
+  //     makeNewTD.addEventListener('click', function (evt) {
+  //       evt.preventDefault();
+  //       emptyBoard();
+  //     });
+  //   }
+  // }
+
+  function fillQuestions(x, y) {
+    // alert(x + ' ' + y);
+    // const makeP = document.createElement('p');
+    const getQuestionDiv = document.getElementById(y.toString() + x.toString());
+    getQuestionDiv.style.fontSize = '1.2rem';
+    getQuestionDiv.innerText = getQuestion[y][x].question;
+    getQuestionDiv.addEventListener('click', function (evt) {
+      evt.preventDefault();
+      const setAnswer = prompt(getQuestion[y][x].question);
+      if (getQuestion[y][x].answer.includes(setAnswer)) {
+        alert('Correct');
+        getQuestionDiv.style.backgroundColor = 'green';
+      } else {
+        getQuestionDiv.style.backgroundColor = 'red';
+      }
+      getQuestionDiv.innerText = getQuestion[y][x].answer;
+    });
+  }
 }
 
 /** Handle clicking on a clue: show the question or answer.
@@ -129,11 +174,23 @@ function handleClick(evt) {}
  * and update the button used to fetch data.
  */
 
-function showLoadingView() {}
+function showLoadingView() {
+  newSpinner = document.createElement('i');
+  newSpinner.className = 'fa-solid fa-arrows-spin fa-spin';
+  newSpinner.style.fontSize = '15rem';
+  spinnerDiv = document.createElement('div');
+  spinnerDiv.className = 'div col-12 text-center';
+  spinnerDiv.id = 'txtLoader';
+  tableDiv.append(spinnerDiv);
+  spinnerDiv.append(newSpinner);
+}
 
 /** Remove the loading spinner and update the button used to fetch data. */
 
-function hideLoadingView() {}
+function hideLoadingView() {
+  const hidDiv = document.querySelector('#txtLoader');
+  hidDiv.style.display = 'none';
+}
 
 /** Start game:
  *
@@ -141,11 +198,21 @@ function hideLoadingView() {}
  * - get data for each category
  * - create HTML table
  * */
-
-async function setupAndStart() {
+function emptyBoard() {
+  getTableDiv = document.querySelector('#tableDiv');
+  if (getTableDiv) {
+    getTableDiv.remove();
+    categories = [];
+  }
+}
+function setupAndStart() {
+  emptyBoard();
   getCategoryIds();
 }
 
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
 /** On click of start / restart button, set up game. */
 
 // TODO
